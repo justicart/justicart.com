df60ed01dd881aad95cdd7e3339a3a7c55738ae0
Justin Carter <github@justicart.com>
2022-01-04 20:23:40 -0500

    first commit

---
+++ src/social.js
@@ -0,0 +1,17 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+export default function() {
+  return (
+    <div className="social">
+      I don't know what to do with this site
+      {/* <FontAwesomeIcon icon={faTwitter} />
+      <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon icon={faInstagram} />
+      <FontAwesomeIcon icon={faLinkedin} />
+      <FontAwesomeIcon icon={faYoutube} />
+      <FontAwesomeIcon icon={faEnvelope} /> */}
+    </div>
+  )
+}
